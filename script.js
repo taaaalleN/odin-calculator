cf4a49a176cd789c5325ffc00523c182674587af
Patrick <taaaallen@gmail.com>
2023-04-10 22:30:31 +0200

    Set up initial script file

---
+++ script.js
@@ -0,0 +1,23 @@
+let first;
+let second;
+let operator;
+
+function add(firstNumber, secondNumber) {
+  return firstNumber + secondNumber;
+}
+
+function subtract(firstNumber, secondNumber) {
+  return firstNumber - secondNumber;
+}
+
+function multiply(firstNumber, secondNumber) {
+  return firstNumber * secondNumber;
+}
+
+function divide(firstNumber, secondNumber) {
+  return firstNumber / secondNumber;
+}
+
+function operate(operator, firstNumber, secondNumber) {
+  operator(firstNumber, secondNumber);
+}
