db699de03e5d41eeb37df438d6250a9aa9cc309d
Patrick <taaaallen@gmail.com>
2023-04-15 23:02:54 +0200

    Add button functionality

--- script.js
+++ script.js
@@ -21,3 +21,20 @@ function divide(firstNumber, secondNumber) {
 function operate(operator, firstNumber, secondNumber) {
   operator(firstNumber, secondNumber);
 }
+
+const numbers = document.querySelectorAll(".buttons .numbers");
+const operators = document.querySelectorAll(".buttons .operators");
+let displayValue = "";
+let display = document.querySelector(".display");
+
+numbers.forEach((number) =>
+  number.addEventListener("click", (e) => {
+    console.log(e.target.innerText);
+    displayValue += e.target.innerText;
+    display.innerText = "";
+    display.innerText = displayValue;
+    console.log(displayValue);
+    // displayValue += number.innerText;
+    // display.innerText = displayValue;
+  })
+);
