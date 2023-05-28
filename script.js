3ccb0d91149dfcf4a1da24099605508760fb57d2
Patrick <taaaallen@gmail.com>
2023-05-28 22:12:00 +0200

    Some changes

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
-const numbers = document.querySelectorAll(".buttons .numbers");
-const operators = document.querySelectorAll(".buttons .operators");
-const display = document.querySelector(".display");
+const numbers = document.querySelectorAll(".buttons .numbers .btn");
+const operators = document.querySelectorAll(".buttons .operators .btn");
+const previousInput = document.querySelector(".display .input.previous");
+const currentInput = document.querySelector(".display .input.current");
+// const display = document.querySelector(".display");
 let displayValue = "";
 let firstNumber = 0;
 let secondNumber = 0;
-let selectedOperator;
+let selectedOperator = "";
 let currentDisplayArray = [];
-let previousInput = "";
+// let previousInput = "";
 
 const regex = /\+|-|\*|\//g;
 
@@ -27,121 +29,81 @@ function divide(firstValue, secondValue) {
 }
 
 function clear() {
-  display.innerText = "";
-  displayValue = "";
+  display.textContent = "";
   firstNumber = "";
   secondNumber = "";
   selectedOperator = "";
 }
 
-let operatorsMap = {
-  add: add,
-  subtract: subtract,
-  multiply: multiply,
-  divide: divide,
-};
-
-function operate(operator, firstValue, secondValue) {
-  const operatorFunction = operatorsMap[operator];
-  if (operatorFunction) {
-    return operatorFunction(firstValue, secondValue);
-  } else {
-    console.error(operator + " is an invalid operator");
-    throw new Error("Invalid operator");
+function operate(operator, first, second) {
+  first = Number(first);
+  second = Number(second);
+  switch (operator) {
+    case "sum":
+      calculate();
+      break;
+    case "add":
+    case "subtract":
+    case "multiply":
+    case "divide":
+      return operator(first, second);
+    case "clear":
+      clear();
+      break;
+    default:
+      throw new Error("Invalid operator");
   }
 }
 
+function handleNumbers(number) {
+  currentInput.textContent += number;
+}
+
+numbers.forEach((number) => number.addEventListener("click", () => handleNumbers(number.textContent)));
+
+function setOperator(operator) {
+  // if (selectedOperator == operator) {
+  //   calculate();
+  // }
+  selectedOperator = operator;
+  firstNumber = currentInput.textContent;
+  currentInput.textContent = "";
+  console.log(selectedOperator);
+  console.log(firstNumber);
+  console.log(secondNumber);
+}
+
+// Display e.g. 1 + 2 on the display (somehow)
+function displayNumbers() {
+  previousInput.textContent = `${firstNumber} ${selectedOperator} ${secondNumber}`;
+}
+
+operators.forEach((operator) => operator.addEventListener("click", () => setOperator(operator.textContent)));
+
+function doOtherStuff() {
+  if (previousInput == currentSymbol) {
+    console.log("currentSymbol", currentSymbol, "previousInput", previousInput);
+    if (!secondNumber) {
+      console.log("secondNumber gets assigned same value as firstNumber");
+      secondNumber = firstNumber;
+    }
+  }
+
+  if (currentValue != "sum") {
+    selectedOperator = currentValue;
+  }
+
+  if (firstNumber && secondNumber) {
+    calculate();
+  }
+
+  previousInput = currentDisplayArray.pop();
+  selectedOperator = "";
+}
+
 function calculate() {
+  secondNumber = display.textContent;
   console.log("Operator", selectedOperator, "\n First number", firstNumber, "\n Second number", secondNumber);
   const answer = operate(selectedOperator, firstNumber, secondNumber);
-  displayValue = answer;
-  display.innerText = displayValue;
-  firstNumber = answer;
-  secondNumber = "";
+  display.innerText = answer;
 }
-
-function handleNumbers() {}
-
-function handleClick(elements) {
-  elements.forEach((element) =>
-    element.addEventListener("click", (e) => {
-      const currentValue = e.target.getAttribute("data-value");
-      if (currentValue == null) {
-        return;
-      }
-
-      if (elements == operators) {
-        const currentSymbol = e.target.innerText;
-        currentDisplayArray.push(currentSymbol);
-
-        if (!firstNumber) {
-          let tempArray = displayValue.split(regex);
-          firstNumber = parseInt(tempArray[0]);
-        } else if (!secondNumber) {
-          console.log("displayValue", displayValue);
-          let tempArray = displayValue.split(regex);
-          secondNumber = parseInt(tempArray[1]);
-        }
-
-        if (!firstNumber && !secondNumber) {
-          console.log("An operator can't be the first input");
-          return;
-        }
-
-        if (previousInput == currentSymbol) {
-          console.log("currentSymbol", currentSymbol, "previousInput", previousInput);
-          if (!secondNumber) {
-            console.log("secondNumber gets assigned same value as firstNumber");
-            secondNumber = firstNumber;
-          }
-        }
-
-        if (currentValue != "sum") {
-          selectedOperator = currentValue;
-        }
-
-        if (firstNumber && secondNumber) {
-          calculate();
-        }
-
-        previousInput = currentDisplayArray.pop();
-
-        switch (currentValue) {
-          case "sum":
-            calculate();
-            break;
-          case "add":
-          case "subtract":
-          case "multiply":
-          case "divide":
-            if (!firstNumber) {
-              firstNumber = 0;
-            }
-            displayValue += currentSymbol;
-            display.innerText = displayValue;
-            break;
-          case "clear":
-            clear();
-            break;
-          default:
-            throw new Error("Invalid operator");
-        }
-      } else {
-        handleNumbers();
-        displayValue += currentValue;
-        display.innerText = displayValue;
-        currentDisplayArray.push(currentValue);
-      }
-      console.log("currentDisplayArray", currentDisplayArray);
-      // console.log("firstNumber", firstNumber);
-      // console.log("secondNumber", secondNumber);
-      // console.log("displayValue", displayValue);
-      // console.log("previousInput", previousInput);
-    })
-  );
-  // console.log(displayValue);
-  // console.log(currentDisplayArray);
-}
-
-handleClick(numbers);
-handleClick(operators);
