ff0ce71663f27d66a52f41fade958db1c9ae0546
Patrick <taaaallen@gmail.com>
2023-06-04 00:13:25 +0200

    Reformat operate function to not be as dynamic Operate function no longer returns operator(first, second) due to error with operator not being a function. There might be a workaround that I'm not aware of. Would be possible to make an object and map through functions but will disregard that option for now.

--- script.js
+++ script.js
@@ -1,38 +1,120 @@
-const numbers = document.querySelectorAll(".buttons .numbers .btn");
-const operators = document.querySelectorAll(".buttons .operators .btn");
+const numbers = document.querySelectorAll(".buttons [data-number]");
+const operators = document.querySelectorAll(".buttons [data-operator]");
+const sumBtn = document.querySelector(".buttons [data-value='sum']");
 const previousInput = document.querySelector(".display .input.previous");
 const currentInput = document.querySelector(".display .input.current");
 // const display = document.querySelector(".display");
-let displayValue = "";
-let firstNumber = 0;
-let secondNumber = 0;
-let selectedOperator = "";
+// let displayValue = "";
+let firstNumber = null;
+let secondNumber = null;
+let selectedOperator = null;
+let selectedOperatorChar = null;
 let currentDisplayArray = [];
 // let previousInput = "";
 
 const regex = /\+|-|\*|\//g;
 
-function add(firstValue, secondValue) {
-  return firstValue + secondValue;
+const allBtns = document.querySelectorAll(".buttons .btn");
+
+allBtns.forEach((btn) => btn.addEventListener("click", () => variableHelper()));
+
+function variableHelper() {
+  console.log("selectedOperator", selectedOperator);
+  console.log("firstNumber", firstNumber);
+  console.log("secondNumber", secondNumber);
+  console.log("currentInput.textContent", currentInput.textContent);
+  console.log("previousInput.textContent", previousInput.textContent);
 }
 
-function subtract(firstValue, secondValue) {
-  return firstValue - secondValue;
+function add(a, b) {
+  // console.log(a, b);
+  // console.log(a + b);
+  return a + b;
 }
 
-function multiply(firstValue, secondValue) {
-  return firstValue * secondValue;
+function subtract(a, b) {
+  return a - b;
 }
 
-function divide(firstValue, secondValue) {
-  return firstValue / secondValue;
+function multiply(a, b) {
+  return a * b;
+}
+
+function divide(a, b) {
+  return a / b;
 }
 
 function clear() {
-  display.textContent = "";
+  currentInput.textContent = "";
+  previousInput.textContent = "";
   firstNumber = "";
   secondNumber = "";
   selectedOperator = "";
+}
+
+function deleteLatestInput() {
+  currentInput.textContent = currentInput.textContent.split("").slice(0, -1);
+}
+
+function convertOperation(operatorSymbol) {
+  switch (operatorSymbol) {
+    case "+":
+      return "add";
+    case "-":
+      return "subtract";
+    case "*":
+      return "multiply";
+    case "/":
+      return "divide";
+    case "=":
+      return "sum";
+    default:
+      console.log("Something went wrong: ", operatorSymbol);
+  }
+}
+
+numbers.forEach((number) => number.addEventListener("click", () => handleNumbers(number.textContent)));
+operators.forEach((operator) => operator.addEventListener("click", () => setOperator(operator.textContent)));
+sumBtn.addEventListener("click", evaluate);
+
+function handleNumbers(number) {
+  currentInput.textContent += number;
+}
+
+function setOperator(operator) {
+  if (firstNumber && selectedOperator == operator) {
+    secondNumber = firstNumber;
+    evaluate();
+  }
+
+  if (operator == "C") {
+    clear();
+    return;
+  }
+  // console.log("previousInput before", previousInput.textContent);
+  // console.log("currentInput before", currentInput.textContent);
+
+  firstNumber = currentInput.textContent;
+  console.log("previousInput", previousInput.textContent);
+
+  if (!firstNumber) {
+    console.log("First input cannot be an operator");
+    return;
+  }
+  selectedOperator = convertOperation(operator);
+  if (operator == "=") {
+  }
+  displayNumbers();
+  // console.log("selectedOperator", selectedOperator);
+  // console.log("firstNumber", firstNumber);
+  // console.log("secondNumber", secondNumber);
+}
+
+// Display e.g. 1 + 2 on the display (somehow)
+function displayNumbers() {
+  currentInput.textContent = "";
+  // previousInput.textContent = `${firstNumber} ${selectedOperator} ${secondNumber}`;
+  previousInput.textContent = `${firstNumber} ${convertToSymbol(selectedOperator)}`;
 }
 
 function operate(operator, first, second) {
@@ -40,13 +122,19 @@ function operate(operator, first, second) {
   second = Number(second);
   switch (operator) {
     case "sum":
-      calculate();
+      // calculate();
+      // Do stuff specific to sum
+      // Don't do the "omväg" of going through setOperator
+      // evaluate();
       break;
     case "add":
+      return add(first, second);
     case "subtract":
+      return subtract(first, second);
     case "multiply":
+      return multiply(first, second);
     case "divide":
-      return operator(first, second);
+      return divide(first, second);
     case "clear":
       clear();
       break;
@@ -55,55 +143,33 @@ function operate(operator, first, second) {
   }
 }
 
-function handleNumbers(number) {
-  currentInput.textContent += number;
-}
-
-numbers.forEach((number) => number.addEventListener("click", () => handleNumbers(number.textContent)));
-
-function setOperator(operator) {
-  // if (selectedOperator == operator) {
-  //   calculate();
-  // }
-  selectedOperator = operator;
-  firstNumber = currentInput.textContent;
-  currentInput.textContent = "";
-  console.log(selectedOperator);
-  console.log(firstNumber);
-  console.log(secondNumber);
-}
-
-// Display e.g. 1 + 2 on the display (somehow)
-function displayNumbers() {
-  previousInput.textContent = `${firstNumber} ${selectedOperator} ${secondNumber}`;
-}
-
-operators.forEach((operator) => operator.addEventListener("click", () => setOperator(operator.textContent)));
-
-function doOtherStuff() {
-  if (previousInput == currentSymbol) {
-    console.log("currentSymbol", currentSymbol, "previousInput", previousInput);
-    if (!secondNumber) {
-      console.log("secondNumber gets assigned same value as firstNumber");
-      secondNumber = firstNumber;
-    }
-  }
-
-  if (currentValue != "sum") {
-    selectedOperator = currentValue;
-  }
-
-  if (firstNumber && secondNumber) {
-    calculate();
-  }
-
-  previousInput = currentDisplayArray.pop();
-  selectedOperator = "";
+function evaluate() {
+  console.log("clicked sum");
+  secondNumber = currentInput.textContent;
+  calculate();
 }
 
 function calculate() {
-  secondNumber = display.textContent;
-  console.log("Operator", selectedOperator, "\n First number", firstNumber, "\n Second number", secondNumber);
+  console.log(selectedOperator);
   const answer = operate(selectedOperator, firstNumber, secondNumber);
-  display.innerText = answer;
+  console.log("answer", answer);
+  currentInput.textContent = "";
+  previousInput.textContent = `${answer}`;
+}
+
+function convertToSymbol() {
+  switch (selectedOperator) {
+    case "add":
+      return "+";
+    case "subtract":
+      return "-";
+    case "multiply":
+      return "*";
+    case "divide":
+      return "/";
+    case "sum":
+      return "=";
+    default:
+      throw new Error("Invalid operator symbol");
+  }
 }
